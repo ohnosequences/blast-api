5345b30ba007e3cde69d17a4dab360bc74d229c1
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-08-22 14:42:29 +0200

    do what the previous commit said

---
+++ src/test/scala/CommandGeneration.scala
@@ -0,0 +1,29 @@
+package ohnosequences.blast.test
+
+import ohnosequences.blast.api._
+import ohnosequences.cosas.typeSets._
+import java.io.File
+
+class CommandGeneration extends org.scalatest.FunSuite {
+
+  val dbFile    = new File("/tmp/buh")
+  val queryFile = new File("/tmp/query")
+  val outFile   = new File("/tmp/blastout")
+
+  test("command generation") {
+
+    val blastnCmd = blastn((
+      blastn.arguments(
+        db(dbFile)       :~:
+        query(queryFile) :~:
+        out(outFile)     :~: ∅
+      ),
+      blastn.defaults
+    ))
+
+    assert {
+      blastnCmd.cmd === Seq("blastn", "-db", "/tmp/buh", "-query", "/tmp/query", "-out", "/tmp/blastout") ++
+        blastn.defaultsAsSeq
+    }
+  }
+}
